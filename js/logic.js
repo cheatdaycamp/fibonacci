91502dac3d9aa9d1f010cc3aa09630026d2ca902
Jonatan <jonakru@hotmail.com>
2020-04-20 00:35:10 +0300

    Milestone 6

--- js/logic.js
+++ js/logic.js
@@ -1,39 +1,75 @@
-
 class CalculateFibonacci {
-	constructor(button, input, span, result) {
-	  this.button = button;
-	  this.input = input;
-	  this.span = span;
-	  this.result = result;
-	  this.value
-	  this.button.addEventListener("click", this.launchSearch);
-	}
-  
-	launchSearch = () => {
-	  let value = this.calculateFibonacci(this.input.value);
-	  this.span.innerText = this.input.value;
-	  this.result.innerText = value;
-	};
-  
-	calculateFibonacci = (num, memo) => {
-	  memo = memo || {};
-	  if (memo[num]) {
-		return memo[num];
-	  }
-	  if (num <= 1) {
-		return 1;
-	  }
-	  return (memo[num] =
-		this.calculateFibonacci(num - 1, memo) + this.calculateFibonacci(num - 2, memo));
-	};
+  constructor(button, input, span, result, cardBody, useServer, spinner) {
+    this.button = button;
+    this.input = input;
+    this.span = span;
+    this.result = result;
+    this.useServer = useServer;
+    this.cardBody = cardBody;
+    this.spinner = spinner;
+    this.response;
+    this.button.addEventListener("click", this.launchSearch);
   }
-  
-  window.onload = (() => {
+
+  launchSearch = async () => {
+    this.cardBody.classList.contains("d-none") &&
+      this.cardBody.classList.remove("d-none");
+    let check = await this.checkServer();
+	this.togglehide(this.spinner)
+	console.log(check);
+    this.result.innerText = check.result;
+    if (this.input.value) {
+      this.span.innerText = this.input.value;
+    } else {
+      this.span.innerText = "0";
+    }
+  };
+
+  togglehide = (element) => {
+    element.classList.toggle('invisible');
+  };
+  checkServer = async () => {
+    this.togglehide(this.spinner);
+    if (this.useServer.checked) {
+      return await this.callServer(this.input.value);
+    } else {
+      return {
+        number: parseInt(this.input.value),
+        result: this.calculateFibonacci(this.input.value),
+        createdDate: Date.now(),
+      };
+    }
+  };
+
+  calculateFibonacci = (n) => {
+    if (n < 2) {
+      return n;
+    }
+    return this.calculateFibonacci(n - 1) + this.calculateFibonacci(n - 2);
+  };
+
+  callServer = async (number) => {
+    let url = `http://localhost:5050/fibonacci/${number}`;
+    this.response = await fetch(url).then((response) => response.json());
+    return this.response;
+  };
+}
+
+window.onload = (() => {
   const button = document.getElementById("calculate-button");
-  const result= document.getElementById("fibonacci-results");
+  const result = document.getElementById("fibonacci-results");
   const input = document.getElementById("calculate-input");
   const fibonacciInput = document.getElementById("fibonacci-input");
-  const fibonacci = new CalculateFibonacci(button, input, fibonacciInput, result);
-
-  console.log(button);
+  const cardBody = document.querySelector(".card-holder");
+  const useServer = document.getElementById("use-server");
+  const spinner = document.getElementById("spinner");
+  const fibonacci = new CalculateFibonacci(
+    button,
+    input,
+    fibonacciInput,
+    result,
+    cardBody,
+	useServer,
+	spinner
+  );
 })();

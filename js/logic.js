274fc7d00a753a2b7d45a8f930a0ade25cb78bda
Jonatan <jonakru@hotmail.com>
2020-04-21 13:16:28 +0300

    Finished Milestone 6

--- js/logic.js
+++ js/logic.js
@@ -7,17 +7,28 @@ class CalculateFibonacci {
     this.useServer = useServer;
     this.cardBody = cardBody;
     this.spinner = spinner;
-    this.response;
+    this.fiboResponse;
     this.button.addEventListener("click", this.launchSearch);
   }
 
   launchSearch = async () => {
-    this.cardBody.classList.contains("d-none") &&
-      this.cardBody.classList.remove("d-none");
-    let check = await this.checkServer();
-	this.togglehide(this.spinner)
-	console.log(check);
-    this.result.innerText = check.result;
+    this.checkCardBodyStatus();
+    let caca = this.checkInputLessThan50();
+    this.toggleVisible(this.spinner); //turn on
+    caca ? await this.checkServer() : this.raiseError();
+    this.toggleVisible(this.spinner); //turn off
+    console.log(this.fiboResponse);
+    if (typeof this.fiboResponse === "string") {
+      this.result.innerText = this.fiboResponse;
+    } else if (this.fiboResponse.result) {
+      this.result.innerText = this.fiboResponse.result
+    } else {
+      this.result.innerText = 'COCA'
+    }
+    this.updateSpanText();
+  };
+
+  updateSpanText = () => {
     if (this.input.value) {
       this.span.innerText = this.input.value;
     } else {
@@ -25,15 +36,29 @@ class CalculateFibonacci {
     }
   };
 
-  togglehide = (element) => {
-    element.classList.toggle('invisible');
+  checkCardBodyStatus = () => {
+    this.cardBody.classList.contains("d-none") &&
+      this.cardBody.classList.remove("d-none");
   };
+
+  checkInputLessThan50 = () => {
+    return this.input.value <= 50 ? true : false;
+  };
+
+  raiseError = () => {
+    console.log("error");
+    this.fiboResponse = { result: "En error Ocurred" };
+  };
+
+  toggleVisible = (element) => {
+    element.classList.toggle("invisible");
+  };
+
   checkServer = async () => {
-    this.togglehide(this.spinner);
     if (this.useServer.checked) {
       return await this.callServer(this.input.value);
     } else {
-      return {
+      this.fiboResponse = {
         number: parseInt(this.input.value),
         result: this.calculateFibonacci(this.input.value),
         createdDate: Date.now(),
@@ -50,8 +75,15 @@ class CalculateFibonacci {
 
   callServer = async (number) => {
     let url = `http://localhost:5050/fibonacci/${number}`;
-    this.response = await fetch(url).then((response) => response.json());
-    return this.response;
+    this.fiboResponse = await fetch(url).then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      } else if (response.status === 400) {
+        return response.text();
+      }
+    });
+
+    return this.fiboResponse;
   };
 }
 
@@ -69,7 +101,7 @@ window.onload = (() => {
     fibonacciInput,
     result,
     cardBody,
-	useServer,
-	spinner
+    useServer,
+    spinner
   );
 })();

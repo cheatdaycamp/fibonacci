9725fb0f6819d05a78ce57fc96463880d412a358
Jonatan <jonakru@hotmail.com>
2020-04-21 23:49:42 +0300

    Control flow of the functions

--- js/logic.js
+++ js/logic.js
@@ -1,32 +1,45 @@
 class CalculateFibonacci {
-  constructor(button, input, span, result, cardBody, useServer, spinner) {
+  constructor(button, input, cardBody, useServer, spinner) {
     this.button = button;
     this.input = input;
-    this.span = span;
-    this.result = result;
     this.useServer = useServer;
     this.cardBody = cardBody;
     this.spinner = spinner;
     this.fiboResponse;
+    this.cardText = this.cardBody.firstElementChild.firstElementChild;
     this.button.addEventListener("click", this.launchSearch);
   }
 
   launchSearch = async () => {
-    this.checkCardBodyStatus();
-    let caca = this.checkInputLessThan50();
-    this.toggleVisible(this.spinner); //turn on
-    caca ? await this.checkServer() : this.raiseError();
-    this.toggleVisible(this.spinner); //turn off
-    console.log(this.fiboResponse);
-    if (typeof this.fiboResponse === "string") {
-      this.result.innerText = this.fiboResponse;
-    } else if (this.fiboResponse.result) {
-      this.result.innerText = this.fiboResponse.result
+    this.toggleVisible(this.cardBody, false);
+    this.toggleVisible(this.spinner, true); //turn on
+    const shouldUseServer = this.shouldUseServer();
+    const isInputLessthan50 = this.checkInputLessThan50();
+
+    if (isInputLessthan50) {
+      if (shouldUseServer) {
+        await this.callServer(this.input.value);
+      } else {
+        this.uselocal();
+      }
     } else {
-      this.result.innerText = 'COCA'
+      this.raiseMax50Error();
     }
-    this.updateSpanText();
+    this.toggleVisible(this.spinner, false); //turn on
+    this.fillText();
+    this.checkCardBodyStatus();
   };
+
+  fillText() {
+    if (typeof this.fiboResponse === "string") {
+      this.cardText.innerHTML = this.fiboResponse;
+    } else if (this.fiboResponse.result) {
+      this.cardText.innerHTML = `The Fibonacci number of <strong>${this.fiboResponse.number}</strong> is:
+      <strong>${this.fiboResponse.result}</strong>`;
+    } else {
+      this.cardText.innerHTML = "COCA";
+    }
+  }
 
   updateSpanText = () => {
     if (this.input.value) {
@@ -45,24 +58,32 @@ class CalculateFibonacci {
     return this.input.value <= 50 ? true : false;
   };
 
-  raiseError = () => {
-    console.log("error");
-    this.fiboResponse = { result: "En error Ocurred" };
+  raiseMax50Error = () => {
+    this.fiboResponse = "Max allowed number is: 50";
   };
 
-  toggleVisible = (element) => {
-    element.classList.toggle("invisible");
-  };
-
-  checkServer = async () => {
-    if (this.useServer.checked) {
-      return await this.callServer(this.input.value);
+  toggleVisible = (element, bool) => {
+    if (bool) {
+      element.classList.remove("d-none");
     } else {
+      element.classList.add("d-none");
+    }
+  };
+
+  shouldUseServer = () => {
+    return this.useServer.checked ? true : false;
+  };
+
+  uselocal = async () => {
+    if (this.input.value >= 0) {
+      let results = this.calculateFibonacci(this.input.value);
       this.fiboResponse = {
         number: parseInt(this.input.value),
-        result: this.calculateFibonacci(this.input.value),
+        result: results,
         createdDate: Date.now(),
       };
+    } else {
+      this.fiboResponse = 'Number must be equal or higher than 0'
     }
   };
 
@@ -75,33 +96,26 @@ class CalculateFibonacci {
 
   callServer = async (number) => {
     let url = `http://localhost:5050/fibonacci/${number}`;
-    this.fiboResponse = await fetch(url).then(async (response) => {
-      if (response.ok) {
-        return response.json();
-      } else if (response.status === 400) {
-        return response.text();
-      }
-    });
-
-    return this.fiboResponse;
+    this.fiboResponse = await fetch(url)
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        } else if (response.status === 400) {
+          return response.text();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        return "Server not in use";
+      });
   };
 }
 
 window.onload = (() => {
   const button = document.getElementById("calculate-button");
-  const result = document.getElementById("fibonacci-results");
   const input = document.getElementById("calculate-input");
-  const fibonacciInput = document.getElementById("fibonacci-input");
   const cardBody = document.querySelector(".card-holder");
   const useServer = document.getElementById("use-server");
   const spinner = document.getElementById("spinner");
-  const fibonacci = new CalculateFibonacci(
-    button,
-    input,
-    fibonacciInput,
-    result,
-    cardBody,
-    useServer,
-    spinner
-  );
+  new CalculateFibonacci(button, input, cardBody, useServer, spinner);
 })();
